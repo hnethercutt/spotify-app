2bcd8ad3cc6f3dc4b7ffc7014206cf358f094104
Hannah <144931749+conranh@users.noreply.github.com>
2026-05-12 18:25:24 -0400

    Styled the spotify sign in button and added their colors as global vars

--- src/components/spotify-login-button.tsx
+++ src/components/spotify-login-button.tsx
@@ -1,9 +1,19 @@
 'use client'
+import Image from 'next/image';
+import './spotify-login-button.css';
 
 export default function SpotifyLoginButton() {
     return (
-        <button onClick={() => window.location.href = '/api/login'}>
-            Login with Spotify
+        <button className="spotify-btn">
+            <Image 
+             className='spotify-logo'
+             onClick={() => window.location.href = '/api/login'}
+             src="spotify-logo-white.svg"
+             width="32"
+             height="32"
+             alt="Sign in with Spotify">
+            </Image>
+            <span>Sign in with Spotify</span>
         </button>
     )
 }
