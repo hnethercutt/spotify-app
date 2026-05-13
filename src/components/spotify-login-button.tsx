d3e2c1a1c09252db8ed767bedd01fb389859c80a
Hannah <144931749+conranh@users.noreply.github.com>
2026-05-13 16:21:48 -0400

    Formatting update

--- src/components/spotify-login-button.tsx
+++ src/components/spotify-login-button.tsx
@@ -1,19 +1,21 @@
-'use client'
+'use client';
 import Image from 'next/image';
 import './spotify-login-button.css';
 
 export default function SpotifyLoginButton() {
-    return (
-        <button className="spotify-btn">
-            <Image 
-             className='spotify-logo'
-             onClick={() => window.location.href = '/api/login'}
-             src="spotify-logo-white.svg"
-             width="32"
-             height="32"
-             alt="Sign in with Spotify">
-            </Image>
-            <span>Sign in with Spotify</span>
-        </button>
-    )
+  return (
+    <button
+      className="spotify-btn"
+      onClick={() => (window.location.href = '/api/login')}
+    >
+      <Image
+        className="spotify-logo"
+        src="spotify-logo-white.svg"
+        width="32"
+        height="32"
+        alt="Sign in with Spotify"
+      ></Image>
+      <span>Sign in with Spotify</span>
+    </button>
+  );
 }
