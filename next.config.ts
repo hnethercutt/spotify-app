a161d8131639a309ba4857ed144576ca7927249d
hnethercutt <conranh23@students.ecu.edu>
2026-05-13 23:56:55 -0400

    Added image config so I can pull album art from spotify

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ['127.0.0.1'],
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'i.scdn.co',
+      port: '',
+      pathname: '/image/**'
+    },],
+  },
 };
 
 export default nextConfig;
