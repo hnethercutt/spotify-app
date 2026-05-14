a5a996fe522ba2ff25d09e4749897f5a51458207
hnethercutt <conranh23@students.ecu.edu>
2026-05-13 23:58:29 -0400

    Added an autocomplete search feature and applied some styling to the search bar and results

--- src/components/spotify-search-bar.tsx
+++ src/components/spotify-search-bar.tsx
@@ -1,50 +1,90 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import _ from 'lodash';
 import './spotify-search-bar.css';
 
 export default function SpotifySearchBar() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState<
-    { track: string; artist: string; album: string; id: string }[]
+    {
+      track: string;
+      artist: string;
+      album: string;
+      cover: string;
+      id: string;
+    }[]
   >([]);
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      if (searchTerm) {
+        try {
+          const res = await fetch(
+            `/api/search?q="${encodeURIComponent(searchTerm)}"`
+          );
+          await res.json().then(function (_searchResult) {
+            let formattedResults = _.map(
+              _searchResult.items,
+              (_searchResultItem) => ({
+                track: _searchResultItem.name,
+                artist: _searchResultItem.artists[0].name,
+                album: _searchResultItem.album.name,
+                cover: _searchResultItem.album.images[0].url,
+                id: _searchResultItem.id,
+              })
+            );
+            setSearchResults(formattedResults);
+            console.log(formattedResults);
+          });
+        } catch (err) {
+          console.error('Error retrieving search results', err);
+        }
+      } else {
+        setSearchResults([]);
+      }
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
 
   const updateSearchTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setSearchTerm(e.target.value);
   };
 
-  const handleSearch = async (e: any) => {
-    e.preventDefault();
-    try {
-      const res = await fetch(
-        `/api/search?q="${encodeURIComponent(searchTerm)}"`
-      );
-      await res.json().then(function(_searchResult) {
-        let formattedResults = _.map(_searchResult.items, (_searchResultItem)  => ({
-          track: _searchResultItem.name,
-          artist: _searchResultItem.artists[0].name,
-          album: _searchResultItem.album.name,
-          id: _searchResultItem.id
-        }));
-        setSearchResults(formattedResults);
-        console.log(formattedResults);
-      });
-    } catch (err) {
-      console.error('Error retrieving search results', err);
-    }
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSearch}>
+    <div className="search-bar-container">
+      <form>
         <input
           type="text"
+          placeholder="Search for a song.."
           value={searchTerm}
           onChange={updateSearchTerm}
+          onBlur={() => {
+            setSearchResults([]);
+          }}
         ></input>
-        <button type="submit">Submit</button>
       </form>
+      {searchResults.length > 0 && (
+        <div className="search-results-container">
+          {searchResults.map((item, index) => (
+            <div key={index} className="search-result">
+              <Image
+                className="cover-art"
+                src={item.cover}
+                width="50"
+                height="50"
+                alt="Album cover"
+              ></Image>
+              <div className="result-text">
+                <span className="track-name">{item.track}</span>
+                <span className="artist-name">Song &sdot; {item.artist}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
