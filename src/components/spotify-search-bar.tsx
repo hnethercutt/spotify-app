743a6644e96feed0fad31b41d81360037994dc3d
hnethercutt <hconran@gmail.com>
2026-09-13 15:21:26 -0400

    Modified search bar to pass selected song data

--- src/components/spotify-search-bar.tsx
+++ src/components/spotify-search-bar.tsx
@@ -1,46 +1,27 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { SpotifySong } from '@/types/playlist';
+import { fetchSearchResults } from '@/services/playlist-service';
 import Image from 'next/image';
 import _ from 'lodash';
 import './spotify-search-bar.css';
 
-export default function SpotifySearchBar() {
+// So parent components can access selected song data
+interface SearchBarProps {
+  onSongSelected: (refSong: SpotifySong) => void;
+}
+
+export default function SpotifySearchBar({ onSongSelected }: SearchBarProps) {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState<
-    {
-      track: string;
-      artist: string;
-      album: string;
-      cover: string;
-      id: string;
-    }[]
-  >([]);
+  const [searchResults, setSearchResults] = useState<SpotifySong[]>([]);
 
   useEffect(() => {
     // Wait a couple seconds while the user types in their search before making the API call
     const timer = setTimeout(async () => {
       if (searchTerm) {
-        try {
-          const res = await fetch(
-            `/api/search?q="${encodeURIComponent(searchTerm)}"`
-          );
-          await res.json().then(function (_searchResult) {
-            // Add a search result object to the array for each result item (each song)
-            let formattedResults = _.map(
-              _searchResult.items,
-              (_searchResultItem) => ({
-                track: _searchResultItem.name,
-                artist: _searchResultItem.artists[0].name,
-                album: _searchResultItem.album.name,
-                cover: _searchResultItem.album.images[0].url,
-                id: _searchResultItem.id,
-              })
-            );
-            setSearchResults(formattedResults);
-          });
-        } catch (err) {
-          console.error('Error retrieving search results', err);
-        }
+        fetchSearchResults(searchTerm).then(function(_searchResults) {
+          setSearchResults(_searchResults);
+        });
       // User either never typed anything in or erased all of what they had, so we want to clear the results
       } else {
         setSearchResults([]);
@@ -57,33 +38,52 @@ export default function SpotifySearchBar() {
     setSearchTerm(e.target.value);
   };
 
+  // Pass back the data for the song that was clicked and clear the results display
+  const handleSongClick = (selectedSong: SpotifySong) => {
+    onSongSelected(selectedSong);
+    setSearchResults([]);
+  };
+
+  const searchRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Hide search results when the user clicks outside of the search bar or results area
+    const handleClickOffResults = (e: MouseEvent) => {
+      if(searchRef.current && !searchRef.current.contains(e.target as Node)) {
+        setSearchResults([]);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOffResults);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOffResults);
+    };
+  }, []);
+
   return (
-    <div className="search-bar-container">
+    <div ref={searchRef} className="search-bar-container">
       <form>
         <input
           type="text"
           placeholder="Search for a song.."
           value={searchTerm}
           onChange={updateSearchTerm}
-          // Hide search results after clicking away
-          onBlur={() => {
-            setSearchResults([]);
-          }}
         ></input>
       </form>
       {searchResults.length > 0 && (
         <div className="search-results-container">
           {searchResults.map((item, index) => (
-            <div key={index} className="search-result">
+            <div key={index} className="search-result" onClick={() => handleSongClick(item)}>
               <Image
                 className="cover-art"
-                src={item.cover}
+                src={item.coverArtUrl}
                 width="50"
                 height="50"
                 alt="Album cover"
               ></Image>
               <div className="result-text">
-                <span className="track-name">{item.track}</span>
+                <span className="track-name">{item.title}</span>
                 <span className="artist-name">Song &sdot; {item.artist}</span>
               </div>
             </div>
