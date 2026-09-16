a6dea1bafdd47b3dd5a4d6e8d95bc37dc4611616
hnethercutt <hconran@gmail.com>
2026-09-16 11:20:58 -0400

    Connected prompt to gemini API to receive playlist songs in console

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,17 +1,15 @@
 import { GoogleGenAI } from '@google/genai';
 import { NextRequest, NextResponse } from 'next/server';
+import { generatePlaylistPrompt } from '@/lib/generatePlaylistPrompt';
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY!,
 });
 
 export async function POST(req: NextRequest) {
-  let prompt = '';
   try {
-    await req.json().then(function(_req) {
-      // Temporarily this is just a user submitted prompt
-      prompt = _req.prompt;
-    });
+    const requestData = await req.json();
+    const prompt = generatePlaylistPrompt(requestData.prompt);
 
     const response = await ai.models.generateContent({
       model: 'gemini-3.1-flash-lite',
